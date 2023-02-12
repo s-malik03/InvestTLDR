a3acbfa3a0df95699a849cb7d4f0d04cabaeef84
s-malik03 <safiy.malik@gmail.com>
2023-02-12 11:46:31 +0000

    add bing api

---
+++ bing_api.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def get_news_articles(company, api_key):
+    query = f'"{company}"'
+    headers = {
+        'Ocp-Apim-Subscription-Key': 'afb43b53c95e45f38abaed82fd388bf7'
+    }
+    params = {
+        'q': query,
+        'count': 10,
+        'mkt': 'en-US'
+    }
+    url = 'https://api.bing.microsoft.com/v7.0/news/search/'
+    response = requests.get(url, headers=headers, params=params)
+    response_json = response.json()
+    articles = response_json['value']
+    return [article['url'] for article in articles]
+
+if __name__ == '__main__':
+    api_key = 'YOUR_API_KEY'
+    company = input('Enter company name: ')
+    urls = get_news_articles(company, api_key)
+    for i, url in enumerate(urls):
+        print(f'{i+1}. {url}')
